1690bf287031ba651727ea84606192f3f9e2648e
Farid Neshat <FaridN_SOAD@yahoo.com>
2019-04-26 15:52:14 +0200

    Take lists as input and ouput a map

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,4 @@
-output "ami_id" {
-  description = "The AMI id result of the search"
-  value = "${data.aws_ami.search.id}"
-}
-
-output "root_device_name" {
-  description = "The device name of the root dev"
-  value = "${data.aws_ami.search.root_device_name}"
-}
-
-output "owner_id" {
-  description = "The owner id of the selected ami"
-  value = "${data.aws_ami.search.owner_id}"
+output "ami_ids" {
+  description = "The AMI ids result of the search"
+  value = "${zipmap(var.os, data.aws_ami.search.*.id)}"
 }
